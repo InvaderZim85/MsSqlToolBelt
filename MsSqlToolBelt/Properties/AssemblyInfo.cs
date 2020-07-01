50ae7c213e2b29a09e752059658975ffe8a4f4c8
Andreas Pouwels <apouwels85@gmail.com>
2020-07-01 16:27:25 +0200

    Update AssemblyInfo.cs
    new release

--- MsSqlToolBelt/Properties/AssemblyInfo.cs
+++ MsSqlToolBelt/Properties/AssemblyInfo.cs
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("20.27.0.895")]
-[assembly: AssemblyFileVersion("20.27.0.895")]
+[assembly: AssemblyVersion("20.27.1.986")]
+[assembly: AssemblyFileVersion("20.27.1.986")]
