a99369da1a811d5546f9a87179f4d370c0b3030e
Andreas Pouwels <andreas.pouwels@emp.de>
2021-08-24 13:12:55 +0200

    Fixed bug in the class generator

--- MsSqlToolBelt/Properties/AssemblyInfo.cs
+++ MsSqlToolBelt/Properties/AssemblyInfo.cs
@@ -49,5 +49,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("21.235.0.797")]
-[assembly: AssemblyFileVersion("21.235.0.797")]
+[assembly: AssemblyVersion("21.236.0.792")]
+[assembly: AssemblyFileVersion("21.236.0.792")]
