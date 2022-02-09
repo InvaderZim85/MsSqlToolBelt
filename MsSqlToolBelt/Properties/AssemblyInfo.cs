a2d9a62a77469e68cb21b1a82f2c6021abe76f27
Andreas Pouwels <andreas.pouwels@emp.de>
2022-02-09 17:29:21 +0100

    Update SqlParser package

--- MsSqlToolBelt/Properties/AssemblyInfo.cs
+++ MsSqlToolBelt/Properties/AssemblyInfo.cs
@@ -49,5 +49,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("22.40.4.1044")]
-[assembly: AssemblyFileVersion("22.40.4.1044")]
+[assembly: AssemblyVersion("22.40.7.1048")]
+[assembly: AssemblyFileVersion("22.40.7.1048")]
